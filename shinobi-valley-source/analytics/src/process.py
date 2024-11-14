4ba79d69c969108e0d24f8612a3f27e9acb57496
Nuno Pereira <nunoafonso2002@gmail.com>
2024-11-14 17:59:07 +0100

    Added more analytic methods

--- shinobi-valley-source/analytics/src/process.py
+++ shinobi-valley-source/analytics/src/process.py
@@ -6,15 +6,30 @@ from models import Userdata, Timeseries
 from pathlib import Path
 
 current_working_directory = Path.cwd()
-print(f"sqlite://{str(current_working_directory.parent.joinpath('server-assets', 'db.db'))}")
-sqlite_engine = create_engine(f"sqlite://{str(current_working_directory.parent.joinpath('server-assets', 'db.db'))}", echo=True)
+sqlite_engine = create_engine(f"sqlite:///{str(current_working_directory.parent.joinpath('server-assets', 'db.db'))}", echo=False)
 
 def perspective_distribution(_session: Session):
     
-    statement = select(Userdata.perspective, func.count().label("num_participants")).group_by(Userdata.perspective)
+    statement = select(Userdata.perspective, func.count().label("num_participants"))\
+        .group_by(Userdata.perspective)
 
-    print(session.scalars(statement).all())
+    return {perspective: count for (perspective, count) in (_session.execute(statement))}
+
+def positions_for_user(_session: Session, user_id):
+    statement = select(Timeseries.logline).where((Timeseries.userdata_id == user_id), (Timeseries.logtype == "POSLOG"))
+
+    return _session.scalars(statement).all()
+
+def collected_items_per_user(_session: Session):
+    statement = select(Timeseries.userdata_id.label("user_id"), func.count(Timeseries.logline).label("num_items"))\
+        .where(Timeseries.logtype == "ITEMPICKUP")\
+        .group_by(Timeseries.userdata_id)
+
+    return {user_id: count for (user_id, count) in (_session.execute(statement))}
+
 
 with Session(sqlite_engine) as session, session.begin():
     
-    perspective_distribution(session)
+    print(perspective_distribution(session))
+    print(positions_for_user(session, 1))
+    print(collected_items_per_user(session))
